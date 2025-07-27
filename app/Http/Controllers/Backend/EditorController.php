e6b3bb43170498fa19f9fd410ba241668123ae86
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2025-07-27 13:46:02 +0600

    Fixed test cases and add strict typing

--- app/Http/Controllers/Backend/EditorController.php
+++ app/Http/Controllers/Backend/EditorController.php
@@ -1,10 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class EditorController extends Controller
