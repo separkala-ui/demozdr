edc1735538414ce3e865857ccfc59cc4a78b228f
engrshishir <shishir.cse.pstu@gmail.com>
2025-07-29 17:04:14 +0600

    Refactor empty state component

--- resources/views/components/table/empty-state.blade.php
+++ resources/views/components/table/empty-state.blade.php
@@ -1,19 +1,38 @@
+@props([
+    'title' => __('No Datas found'),
+    'description' => __('Get started by creating new data.'),
+    'action' => null,
+    'actionId' => null,
+    'actionLabel' => __('Create'),
+])
+
 <div class="bg-white rounded-lg p-6 flex flex-col items-center justify-center text-center">
 
     <iconify-icon icon="bi:megaphone" class="text-gray-300 dark:text-gray-600 text-6xl mb-4"></iconify-icon>
-    <div class="font-semibold crm:text-lg text-gray-800 mb-1">
-        {{ $title ?? __('No Datas found') }}
-    </div>
-    <div class="text-gray-500 mb-4">
-        {{ $description ?? __('Get started by creating your first email campaign.') }}
-    </div>
-    @if (!empty($actionUrl) && $actionUrl !=='')
-        <a href="{{ $actionUrl }}"
-            class="btn btn-primary">
+
+    @if (!empty($title))
+        <div class="font-semibold crm:text-lg text-gray-800 mb-1">
+            {{ $title }}
+        </div>
+    @endif
+
+    @if (!empty($description))
+        <div class="text-gray-500 mb-4">
+            {{ $description }}
+        </div>
+    @endif
+
+    @if ($actionId)
+        <button type="button"
+            onclick="if(typeof openDrawer === 'function'){ openDrawer('{{ $actionId }}'); } else { window.dispatchEvent(new CustomEvent('open-drawer-{{ $actionId }}')); } return false;"
+            class="btn btn-primary flex items-center">
             <iconify-icon icon="feather:plus" class="crm:mr-2 crm:mt-1"></iconify-icon>
-            {{ $actionLabel ?? __('Create') }}
+            {{ $actionLabel }}
+        </button>
+    @elseif ($action)
+        <a href="{{ $action }}" class="btn btn-primary flex items-center">
+            <iconify-icon icon="feather:plus" class="crm:mr-2 crm:mt-1"></iconify-icon>
+            {{ $actionLabel }}
         </a>
-    @elseif (!empty($action))
-        {{ $action }}
     @endif
 </div>
