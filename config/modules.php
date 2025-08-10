9b0dc825ce6f94efdd03e52621940129d069c019
Mostafizur Rahman <mostafizurhimself@gmail.com>
2025-08-10 11:41:34 +0600

    refactor: rename modules directory

--- config/modules.php
+++ config/modules.php
@@ -85,7 +85,7 @@ return [
         | This path will also be added automatically to the list of scanned folders.
         |
         */
-        'modules' => base_path('Modules'),
+        'modules' => base_path('modules'),
 
         /*
         |--------------------------------------------------------------------------
