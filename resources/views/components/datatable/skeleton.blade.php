c61a260509443e864d926e12c74bc146c275d494
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2025-08-29 02:24:19 +0600

    refactor: improved table skeleton loader

--- resources/views/components/datatable/skeleton.blade.php
+++ resources/views/components/datatable/skeleton.blade.php
@@ -4,11 +4,13 @@
     'enableCheckbox' => true
 ])
 
-<div>
+<div class="table-responsive">
     <table class="table">
         <thead class="table-thead">
-            <tr class="table-tr flex justify-between">
+            <tr class="table-tr">
+                @if($enableCheckbox)
                 <th width="3%" class="table-thead-th"><div class="flex items-center"><div class="h-4 w-4 bg-gray-300 dark:bg-gray-700 rounded"></div></div></th>
+                @endif
                 @foreach(range(1, $columns) as $column)
                     <th class="table-thead-th">
                         <div class="flex items-center">
@@ -21,6 +23,12 @@
         <tbody>
             @foreach(range(1, $rows) as $row)
                 <tr class="animate-pulse bg-gray-100 dark:bg-gray-800">
+                    @if($enableCheckbox)
+                    <td class="table-td">
+                        <div class="h-4 w-4 bg-gray-300 dark:bg-gray-700 rounded"></div>
+                    </td>
+                    @endif
+
                     @foreach(range(1, $columns) as $column)
                         <td class="table-td">
                             <div class="h-4 w-24 bg-gray-300 dark:bg-gray-700 rounded mb-1"></div>
