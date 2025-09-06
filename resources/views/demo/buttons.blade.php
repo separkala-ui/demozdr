b2e7dce53f77aa7d1d0742888d2015ee20f96db5
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2025-09-06 10:59:40 +0600

    feat: update buttons components to support loading and some more props

--- resources/views/demo/buttons.blade.php
+++ resources/views/demo/buttons.blade.php
@@ -4,20 +4,27 @@
 
 <div class="flex flex-col gap-5">
     <x-demo.preview-component
-        title="{{ __('Normal Button') }}"
+        title="{{ __('Normal Buttons') }}"
         description="{{ __('Reusable button component supporting type and custom classes.') }}"
         path="views/demo/buttons/button.blade.php"
         include="demo.buttons.button"
     />
+
     <x-demo.preview-component
-        title="{{ __('Action Buttons') }}"
+        title="{{ __('Loading Buttons') }}"
+        path="views/demo/buttons/loading-buttons.blade.php"
+        include="demo.buttons.loading-buttons"
+    />
+
+    <x-demo.preview-component
+        title="{{ __('Action Button') }}"
         description="{{ __('Common action buttons for CRUD and navigation.') }}"
         path="views/demo/buttons/action-buttons.blade.php"
         include="demo.buttons.action-buttons"
     />
 
     <x-demo.preview-component
-        title="{{ __('Submit Buttons') }}"
+        title="{{ __('Submit Button') }}"
         path="views/demo/buttons/submit-buttons.blade.php"
         include="demo.buttons.submit-buttons"
     />
